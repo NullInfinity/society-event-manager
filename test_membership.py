4c7e72797792484b6edf35c55a11b1c8435eda94
Alex Thorne <alex@alexthorne.net>
2016-08-18 01:39:21 +0100

    Combine all module tests in single file

--- test_membership.py
+++ test_membership.py
@@ -4,6 +4,85 @@ from datetime import date
 from unittest.mock import patch, call
 from membership import Name, Member, MemberDatabase
 
+class NameTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.first_name = 'Ted'
+        self.middle_name = 'Rogers'
+        self.last_name = 'Bobson'
+        self.empty_name = Name()
+        self.short_name = Name(self.last_name)
+        self.name = Name(self.first_name, self.last_name)
+        self.long_name = Name(self.first_name, self.middle_name, self.last_name)
+
+    def test_falsiness(self):
+        self.assertFalse(self.empty_name)
+        self.assertTrue(self.short_name)
+        self.assertTrue(self.name)
+        self.assertTrue(self.long_name)
+
+    def test_first(self):
+        self.assertEqual('', self.empty_name.first())
+        self.assertEqual('', self.short_name.first())
+        self.assertEqual(self.first_name, self.name.first())
+        self.assertEqual(self.first_name + ' ' + self.middle_name, self.long_name.first())
+
+    def test_last(self):
+        self.assertEqual('', self.empty_name.last())
+        self.assertEqual(self.last_name, self.short_name.last())
+        self.assertEqual(self.last_name, self.name.last())
+        self.assertEqual(self.last_name, self.long_name.last())
+
+    def test_full(self):
+        self.assertEqual('', self.empty_name.full())
+        self.assertEqual(self.last_name, self.short_name.full())
+        self.assertEqual(self.first_name + ' ' + self.last_name, self.name.full())
+        self.assertEqual(self.first_name + ' ' + self.middle_name + ' ' + self.last_name, self.long_name.full())
+
+    def test_equality(self):
+        self.assertEqual(self.empty_name, Name())
+        self.assertEqual(self.short_name, Name(self.last_name))
+        self.assertEqual(self.name, Name(self.first_name, self.last_name))
+        self.assertEqual(self.long_name, Name(self.first_name, self.middle_name, self.last_name))
+
+    def test_inequality(self):
+        self.assertNotEqual(self.empty_name, self.short_name)
+        self.assertNotEqual(self.empty_name, self.name)
+        self.assertNotEqual(self.empty_name, self.long_name)
+        self.assertNotEqual(self.short_name, self.name)
+        self.assertNotEqual(self.short_name, self.long_name)
+        self.assertNotEqual(self.name, self.long_name)
+
+class MemberTestCase(unittest.TestCase):
+    def setUp(self):
+        self.barcode = '00000000'
+        self.name = Name('Ted', 'Bobson')
+        self.college = 'Wolfson'
+
+    def test_falsiness(self):
+        self.assertTrue(Member(self.barcode, name=self.name))
+        self.assertTrue(Member(self.barcode, name=self.name, college=self.college))
+        self.assertTrue(Member(self.barcode))
+        self.assertTrue(Member(None, name=self.name))
+
+        self.assertFalse(Member(None)) # name=None by default
+        self.assertFalse(Member(None, college=self.college))
+
+    def test_equality(self):
+        self.assertEqual(Member(self.barcode, name=self.name), Member(self.barcode, name=self.name))
+        self.assertEqual(Member(self.barcode), Member(self.barcode))
+        self.assertEqual(Member(None, self.name), Member(None, self.name))
+
+    def test_inequality(self):
+        self.assertNotEqual(Member(None, name=self.name), Member(self.barcode))
+        self.assertNotEqual(Member(self.barcode, name=self.name), Member(self.barcode))
+        self.assertNotEqual(Member(self.barcode, name=self.name), Member('11111111', name=self.name))
+
+    def test_varargs_init(self):
+        self.assertEqual(Member(None, name=self.name), Member(None, self.name.first(), self.name.last()))
+        self.assertEqual(Member(self.barcode, name=Name('Ted', 'Bobson', 'Jones')), Member(self.barcode, 'Ted', 'Bobson', 'Jones'))
+        self.assertEqual(Member(self.barcode, name=self.name), Member(self.barcode, 'Billy', name=self.name))
+
 class MemberDatabaseTestCase(unittest.TestCase):
     def setUp(self):
         self.first_name = 'Ted'
