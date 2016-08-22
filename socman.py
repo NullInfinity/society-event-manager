86d6aa7131abf491166288bb4dc1438f05ba8b73
Alex Thorne <alex@alexthorne.net>
2016-08-22 23:20:22 +0100

    Updates to frontend

--- socman.py
+++ socman.py
@@ -1,20 +1,32 @@
-#!/usr/bin/python3
-import membership
+#!/usr/bin/env python3
+from membership import Name, Member, MemberDatabase
 from datetime import date
 
-app_name = "socman.py"
-app_version = "0.1"
+APP_NAME = "socman.py"
+APP_VERSION = "0.1"
+
+class Event:
+    def __init__(self, name):
+        self.name = name
+        self.date = date.today()
+        self.one_time = 0
+        self.sign_ups = 0
+        self.members_present = 0
+
+    def total_attendees(self):
+        return self.one_time + self.sign_ups + self.members_present
+
+    def __str__(self):
+        return 'There were {0} attendees, of which {1} were existing members, {2} were newly signed up members and {3} paid a one time entry fee.'.format(self.total_attendees(), self.members_present, self.sign_ups, self.one_time)
 
 event_name = input('Please enter event name: ')
-event_date = date.today()
 print('The date is {0}.'.format(event_date))
 print()
-
 print('Proceed to check membership and attendance will be tracked automatically.')
-mdb = membership.MemberDatabase('members.db')
-oneTime = 0
-signUps = 0
-membersPresent = 0
+
+DATABASE_FILE = 'members.db'
+mdb = MemberDatabase(DATABASE_FILE)
+
 
 def checkMember(barcode):
     # check member database for barcode
@@ -43,7 +55,7 @@ while True:
     if barcode == 'NOT' or not checkMember():
         # add member?
         pass
-        #offer to add, make onetime sale, or cancel
+    #offer to add, make onetime sale, or cancel
         print('Not a member. You may either\n<ADD> a new member\nMake a <ONE>time sale, or\n<CANCEL>')
         response = input()
         if response == 'ADD':
@@ -54,9 +66,6 @@ while True:
             oneTime = oneTime + 1
 
 # wrap up
-totalAttendees = oneTime + signUps + membersPresent
-print()
-print('There were {0} attendees, of which {1} were existing members, {2} were newly signed up members and {3} paid a one time entry fee.'.format(totalAttendees, membersPresent, signUps, oneTime))
 
 # now record this information
 print('This information has been recorded. Thank you for using the society event manager.')
