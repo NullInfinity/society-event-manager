c469cbb394df6554ead82be999489f96a9847a5b
Alex Thorne <alex@alexthorne.net>
2016-08-26 09:48:51 +0100

    Add description and license to module docstring

--- socman.py
+++ socman.py
@@ -1,7 +1,32 @@
 """
-socman society membership library
+socman is a society membership and event management library.
 
-tracks and verifies society membership in a sqlite database"""
+It tracks members in a sqlite database to enable membership tracking
+and verification. It can also track event data such as name and date,
+along with event attendance statistics.
+
+The MIT License (MIT)
+
+Copyright (c) 2016 Alexander Thorne
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
 
 import sqlite3
 from datetime import date, datetime
