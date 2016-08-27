02adf8ce41506ede3e1a39cecf70e96056f94137
Alex Thorne <alex@alexthorne.net>
2016-08-27 09:28:12 +0100

    Make MemberDatabase helper methods private

--- socman.py
+++ socman.py
@@ -145,12 +145,19 @@ class MemberDatabase:
     # this means users can optionally disable autocommiting for potentially
     # better performance at the cost of reduced data safety on crashes
     def optional_commit(self):
-        """Commits changes to database if `safe` is set to `True`"""
+
+        """Commits changes to database if `safe` is set to `True`.
+
+        This means increased performance can be chosen over the highest level
+        of write safety. For example, by default a timestamp is updated in the
+        database every time a record is accessed. If a lot of members are
+        checked, it may not be desirable to commit after every such change.
+        """
 
         if self.__safe:
             self.__connection.commit()
 
-    def sql_build_name_value_pairs(self, member, sep):
+    def __sql_build_name_value_pairs(self, member, sep):
         if not member.name:
             return None, None
         columns = []
@@ -165,64 +172,64 @@ class MemberDatabase:
             return None, None
         return sep.join(columns), values
 
-    def sql_search_barcode_phrase(self, member):
+    def __sql_search_barcode_phrase(self, member):
         if not member.barcode:
             return None, None
         return 'barcode=?', (member.barcode, )
 
-    def sql_search_name_phrase(self, member):
+    def __sql_search_name_phrase(self, member):
         if not member.name:
             return None, None
-        return self.sql_build_name_value_pairs(member, ' AND ')
+        return self.__sql_build_name_value_pairs(member, ' AND ')
 
-    def sql_search_phrase(self, member):
-        phrase, values = self.sql_search_barcode_phrase(member)
+    def __sql_search_phrase(self, member):
+        phrase, values = self.__sql_search_barcode_phrase(member)
         if not phrase:
-            phrase, values = self.sql_search_name_phrase(member)
+            phrase, values = self.__sql_search_name_phrase(member)
         return phrase, values
 
-    def sql_update_barcode_phrase(self, member):
+    def __sql_update_barcode_phrase(self, member):
         if not member.barcode:
             return None, None
         return ('barcode=?', (member.barcode,) )
 
-    def sql_update_name_phrase(self, member):
-        return self.sql_build_name_value_pairs(member, ',')
+    def __sql_update_name_phrase(self, member):
+        return self.__sql_build_name_value_pairs(member, ',')
 
-    def sql_search_query(self, member):
-        phrase, values = self.sql_search_phrase(member)
+    def __sql_search_query(self, member):
+        phrase, values = self.__sql_search_phrase(member)
         if not phrase:
             return None, None
         return ('SELECT firstName,lastName FROM users WHERE ' + phrase, values)
 
-    def sql_search_barcode_query(self, member):
-        phrase, values = self.sql_search_barcode_phrase(member)
+    def __sql_search_barcode_query(self, member):
+        phrase, values = self.__sql_search_barcode_phrase(member)
         if not phrase:
             return None, None
         return ('SELECT firstName,lastName FROM users WHERE ' + phrase, values)
 
-    def sql_search_name_query(self, member):
-        phrase, values = self.sql_search_name_phrase(member)
+    def __sql_search_name_query(self, member):
+        phrase, values = self.__sql_search_name_phrase(member)
         if not phrase:
             return None, None
         return ('SELECT firstName,lastName FROM users WHERE ' + phrase, values)
 
-    def sql_update_barcode_query(self, member):
-        set_phrase, set_values = self.sql_update_barcode_phrase(member)
-        search_phrase, search_values = self.sql_search_name_phrase(member)
+    def __sql_update_barcode_query(self, member):
+        set_phrase, set_values = self.__sql_update_barcode_phrase(member)
+        search_phrase, search_values = self.__sql_search_name_phrase(member)
         if not set_phrase or not search_phrase:
             return None, None
         return ('UPDATE users SET ' + set_phrase + ' WHERE ' + search_phrase, set_values + search_values)
 
-    def sql_update_name_query(self, member):
-        set_phrase, set_values = self.sql_update_name_phrase(member)
-        search_phrase, search_values = self.sql_search_barcode_phrase(member)
+    def __sql_update_name_query(self, member):
+        set_phrase, set_values = self.__sql_update_name_phrase(member)
+        search_phrase, search_values = self.__sql_search_barcode_phrase(member)
         if not set_phrase or not search_phrase:
             return None, None
         return ('UPDATE users SET ' + set_phrase + ' WHERE ' + search_phrase, set_values + search_values)
 
-    def sql_update_last_attended_query(self, member):
-        search_phrase, search_values = self.sql_search_phrase(member)
+    def __sql_update_last_attended_query(self, member):
+        search_phrase, search_values = self.__sql_search_phrase(member)
         if not search_phrase:
             return None, None
         return ('UPDATE users SET last_attended=? WHERE ' + search_phrase, (date.today(),) + search_values)
@@ -236,20 +243,20 @@ class MemberDatabase:
             member = Member(barcode)
 
         # first try to find member by barcode, if available
-        query, values = self.sql_search_barcode_query(member)
+        query, values = self.__sql_search_barcode_query(member)
         if query:
             c.execute(query, values)
             users = c.fetchall()
             if users:
                 # if necessary update last_attended date
                 if update_timestamp:
-                    ts_query, ts_values = self.sql_update_last_attended_query(member)
+                    ts_query, ts_values = self.__sql_update_last_attended_query(member)
                     if ts_query:
                         c.execute(ts_query, ts_values)
 
                 # if autofix is set and both names are provided, correct names
                 if autofix:
-                    af_query, af_values = self.sql_update_name_query(member)
+                    af_query, af_values = self.__sql_update_name_query(member)
                     if af_query:
                         c.execute(af_query, af_values)
 
@@ -260,7 +267,7 @@ class MemberDatabase:
                 return users[0]
 
         # barcode lookup failed; now try finding by name
-        query, values = self.sql_search_name_query(member)
+        query, values = self.__sql_search_name_query(member)
         if not query:
             return None
 
@@ -274,19 +281,19 @@ class MemberDatabase:
         if update_timestamp:
             # todo find better way to choose search phrase (barcode/name)
             temp_member = member._replace(barcode = None)
-            ts_query, ts_values = self.sql_update_last_attended_query(temp_member)
+            ts_query, ts_values = self.__sql_update_last_attended_query(temp_member)
             if ts_query:
                 c.execute(ts_query, ts_values)
         # and barcode if autofix is set
         if autofix and member.barcode:
-            af_query, af_values = self.sql_update_barcode_query(member)
+            af_query, af_values = self.__sql_update_barcode_query(member)
             c.execute(af_query, af_values)
             self.optional_commit()
 
         # TODO dedupe if necessary
         return users[0]
 
-    def sql_add_query(self, member):
+    def __sql_add_query(self, member):
         return 'INSERT INTO users (barcode, firstName, lastName, college, datejoined, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?, ?)', (member.barcode, member.name.first(), member.name.last(), member.college, date.today(), datetime.utcnow(), datetime.utcnow())
 
     def add_member(self, member):
@@ -298,7 +305,7 @@ class MemberDatabase:
 
         # if member does not exist, add them
         cursor = self.__connection.cursor()
-        cursor.execute(*self.sql_add_query(member))
+        cursor.execute(*self.__sql_add_query(member))
         self.__connection.commit()  # direct commit here: don't want to lose new member data
 
         return True
