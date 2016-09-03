ab8e82a4ce80853d2680689da31267cff7189a78
Alex Thorne <alex@alexthorne.net>
2016-09-03 16:39:27 +0100

    Tweak logic in get_member

--- socman.py
+++ socman.py
@@ -303,7 +303,7 @@ class MemberDatabase:
         # TODO implement deduping and better handling of duplicate records
         cursor = self.__connection.cursor()
 
-        if not member or not member.barcode and not member.name:
+        if not member or (not member.barcode and not member.name):
             raise BadMemberError(member)
 
         # first try to find member by barcode, if available
@@ -352,7 +352,8 @@ class MemberDatabase:
         if autofix and member.barcode:
             af_query, af_values = self.__sql_update_barcode_query(member)
             cursor.execute(af_query, af_values)
-            self.optional_commit()
+
+        self.optional_commit()
 
         # TODO dedupe if necessary
         return users[0]
